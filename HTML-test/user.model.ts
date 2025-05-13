8716287fc857a0efe1a32f64d9ce1623dd1f24b0
ShrenikReddy <shrenik.reddy@wenable.com>
2025-05-13 15:39:30 +0530

    Add files via upload

---
+++ HTML-test/user.model.ts
@@ -0,0 +1,19 @@
+export class Person {
+  constructor(
+    public id: number,
+    public firstName: string,
+    public lastName: string
+  ) {}
+}
+
+export class User extends Person {
+
+  constructor(
+    id: number,
+    firstName: string,
+    lastName: string,
+    public email: string
+  ) {
+    super(id, firstName, lastName);
+  }
+}

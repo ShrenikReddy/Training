f499f3539ccf9435aa3d23ecefac77daac10c3c0
ShrenkReddy <shrenik.reddy@wenable.com>
2025-05-16 11:34:00 +0530

    ai generated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'myAngularProject' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myAngularProject');
+   ;
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+
 })
 export class AppComponent {
-  title = 'myAngularProject';
+  nm = '';
+  em = '';
+  ph = '';
+  add = '';
+  submitted: boolean = false;
+  showHeading: boolean = true;
+  qualification = [{ school : '', degree: '', year: ''}];
+
+  addQualification(){
+    this.qualification.push({ school:'', degree:'', year:'' });
+  }
+  formSubmit(){
+    this.submitted = true;
+    this.showHeading = false;
+  }
+  formEdit(){
+    this.submitted = false;
+    this.showHeading = true;
+  }
 }
+
+
